e4c2db58f4d7f86cc5d9fcae1ccc8ab24880d832
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-04-01 15:11:03 +0430

    bug fix(proxy server completed)

--- CustomProxy.py
+++ CustomProxy.py
@@ -4,8 +4,8 @@ from threading import Thread
 
 class CustomProxy():
 
-    def __init__(self, ip="127.0.0.1", backlog=10):
-        self.BUFFER_SIZE = 1024
+    def __init__(self, ip="127.0.0.1", backlog=20):
+        self.BUFFER_SIZE = 2 * 1024
         self.ip = ip
         self.port = 8080
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,24 +20,27 @@ class CustomProxy():
             client_socket, client_address = self.socket.accept()
             log('Accepted a request from client %s!\n' % str(client_address))
             thread = Thread(target=self.handle_client, args=(client_socket, client_address))
-            log("here")
             thread.setDaemon(True)
             thread.start()
 
     def handle_client(self, client_socket, client_address):
-        request = self.recv_all(client_socket)
+        request = client_socket.recv(self.BUFFER_SIZE)
         log("Request received from %s" % str(client_address))
         method, path, host_name, host_port = self.parse_request(request)
         request = self.update_request(request, method, path)
         response = self.send_request(request, host_name, host_port)
-        client_socket.sendall(response.encode('utf-8', 'ignore'))
+        client_socket.sendall(response)
         log("Response sent to client %s" % str(client_address))
 
     def parse_request(self, request):
         splitted_request = request.split('\r\n')
 
         method, requested_address, _ = splitted_request[0].split(' ')
-        host = splitted_request[1].split(' ')[1]
+        host = ''
+        for line in splitted_request:
+            if line.find('Host') != -1:
+                host = line.split(' ')[1]
+                break
         path = requested_address[requested_address.find(host) + len(host):]
         splitted_host = host.split(":")
         host_name = splitted_host[0]
@@ -65,25 +68,26 @@ class CustomProxy():
     def send_request(self, request, host_name, host_port):
         _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         log("Socket to server created")
+        log((host_name, host_port))
         _socket.connect((host_name, host_port))
         log("Socket connected to server")
         log(request)
-        _socket.sendall(request.encode('utf-8', 'ignore'))
+        _socket.sendall(request)
         log("request sent to server")
         response = self.recv_all(_socket)
-        log(response)
         log("response received")
         _socket.close()
         return response
 
     def recv_all(self, _socket):
-        output = b''
+        output = ''
         while True:
             data = _socket.recv(self.BUFFER_SIZE)
-            output += data
-            if len(data) < self.BUFFER_SIZE:
+            if len(data) > 0:
+                output += data
+            else:
                 break
-        return output.decode('utf-8', 'ignore')
+        return output
 
 
 def log(message):
